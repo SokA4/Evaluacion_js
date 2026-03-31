9f65940f72b8be406a0f318c4fa1302229597611
SokA4 <Luisecheverria@liceovvh.cl>
2026-03-31 12:52:36 -0300

    avance

--- evaluacion_js/static/js/script.js
+++ evaluacion_js/static/js/script.js
@@ -1,3 +1,35 @@
+/*1. Módulo de Tesorería: Pago de Permisos (Cálculos y Retorno)
+El vecino ingresa el monto de su permiso de circulación para saber cuánto pagará tras el descuento municipal.
+Lógica: Si el monto es mayor a $70.000, el descuento es del 15%. Si es menor, es del 5%.
+Regla: La Función Principal debe mostrar el total a pagar con un signo de peso ($).
+ */
+
+function moduloTesoreria() {
+    let pago = pagosPermisos(permiso)
+    let monto = parseFloat(input.value)
+    const container = document.getElementById("resultadoContainer1");
+    const result = document.getElementById("resultado1");
+    const input = document.getElementById("input1");
+
+    let descuento = 0;
+    
+    if (pago > 70000) {
+        descuento = pago * 0.15; // 15% de descuento
+    } else {
+        descuento = pago * 0.05; // 5% de descuento
+    }
+
+    let total = pago - descuento;
+    alert(`Su monto a pagar es de $${total}`);
+}
+
+
+
+/*2. Módulo de Salud: Fila de Vacunación (Arreglos y Prioridad)
+Gestión de nombres en la fila de espera del consultorio local.
+Lógica: * Botón Normal: Agrega el nombre al final de la lista (push).
+Botón Urgencia: Agrega el nombre al inicio de la lista (unshift).
+Regla: El resultado debe mostrarse como una lista ordenada en el HTML. <ol>*/
 
 let pacientes = ["Carlos", "Maria", "Diego"];
 function agregarUrgencia(nombre) {
@@ -19,3 +51,32 @@ function ejercicio_2() {
         alert("No puede estar vacío");
     };
 }
+
+let lista = [];
+function agregarLista(nombre) {
+    pacientes.push(nombre);
+    return pacientes.join(", ");
+};
+
+function ejercicio_02() {
+    const container = document.getElementById("resultadoContainer2");
+    const result = document.getElementById("resultado2");
+    const input = document.getElementById("input2");
+    if (input.value !== "") {
+        let nombre = input.value;
+        let resultado = agregarLista(nombre);
+        result.textContent = resultado;
+        input.value = "";
+        container.classList.remove("d-none");
+    } else {
+        alert("No puede estar vacío");
+    };
+}
+
+
+
+/* 
+3. Módulo de Subsidios: Buscador de Beneficiarios (Ciclos e If)
+Verificar si un RUT o nombre está en el listado de entrega de beneficios.
+Lógica: Tener un arreglo con al menos 6 nombres. Usar un ciclo for para buscar si el dato ingresado existe en la lista.
+Regla: La función debe devolver "Beneficiario Verificado" o "No registrado".*/
